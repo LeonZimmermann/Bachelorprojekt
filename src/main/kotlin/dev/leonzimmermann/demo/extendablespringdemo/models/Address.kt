3d1998a6443ed06d7243ee07390743318ef13359
Leon Zimmermann <leon.zimmermann@hotmail.com>
2022-09-29 22:45:37 +0200

    changed entities

--- src/main/kotlin/dev/leonzimmermann/demo/extendablespringdemo/models/Address.kt
+++ src/main/kotlin/dev/leonzimmermann/demo/extendablespringdemo/models/Address.kt
@@ -5,10 +5,10 @@ import javax.persistence.*
 @Entity
 @Table(name = "address")
 data class Address(
-  @Id @GeneratedValue val id: Int? = null,
-  @Column(name = "street") val street: String? = null,
-  @Column(name = "city") val city: String? = null,
-  @Column(name = "state") val state: String? = null,
-  @Column(name = "postalCode") val postalCode: Int? = null,
-  @Column(name = "country") val country: String? = null,
+  @Id @GeneratedValue val id: Long = 0,
+  @Column(name = "street") val street: String,
+  @Column(name = "city") val city: String,
+  @Column(name = "state") val state: String,
+  @Column(name = "postalCode") val postalCode: Int,
+  @Column(name = "country") val country: String,
 )
--- src/main/kotlin/dev/leonzimmermann/demo/extendablespringdemo/models/Person.kt
+++ src/main/kotlin/dev/leonzimmermann/demo/extendablespringdemo/models/Person.kt
@@ -5,7 +5,7 @@ import javax.persistence.*
 @Entity
 @Table(name = "Person")
 data class Person(
-  @Id @GeneratedValue var id: Int,
+  @Id @GeneratedValue var id: Long,
   @Column(name = "firstname", nullable = false) var firstname: String,
   @Column(name = "lastname", nullable = false) var lastname: String,
   @OneToOne(optional = false) @MapsId var address: Address
